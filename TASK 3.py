67bcc8e09af5c139d590ce15816f858e0764141b
Ramya Devi <155082119+ramyadevi2809@users.noreply.github.com>
2024-02-18 12:19:10 +0530

    Add files via upload
    As an Intern in #codsoft , I have created PASSWORD GENERATOTR in PYTHON and successfully completed my TASK 3
    @codsoft

---
+++ TASK 3.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+    print("Password Generator")
+    length = int(input("Enter the desired length of the password: "))
+    if length <= 0:
+        print("Invalid length. Please enter a positive integer.")
+        return
+
+    password = generate_password(length)
+    print("Generated Password:", password)
+
+if __name__ == "__main__":
+    main()
